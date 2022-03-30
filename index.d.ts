8087dcc230846c91be6a22b6f79446546c59f88d
Apehum <apehumchik@gmail.com>
2022-03-31 07:54:32 +0800

    optional scroll behavior

--- index.d.ts
+++ index.d.ts
@@ -54,12 +54,12 @@ export interface ScrollbarProps extends React.HTMLProps<Scrollbars> {
 type ScrollBehavior = "auto" | "smooth" | "instant";
 
 export class Scrollbars extends React.Component<ScrollbarProps> {
-    scrollTop(top: number, scrollBehavior: ScrollBehavior): void;
-    scrollLeft(left: number, scrollBehavior: ScrollBehavior): void;
-    scrollToTop(scrollBehavior: ScrollBehavior): void;
-    scrollToBottom(scrollBehavior: ScrollBehavior): void;
-    scrollToLeft(scrollBehavior: ScrollBehavior): void;
-    scrollToRight(scrollBehavior: ScrollBehavior): void;
+    scrollTop(top: number, scrollBehavior?: ScrollBehavior): void;
+    scrollLeft(left: number, scrollBehavior?: ScrollBehavior): void;
+    scrollToTop(scrollBehavior?: ScrollBehavior): void;
+    scrollToBottom(scrollBehavior?: ScrollBehavior): void;
+    scrollToLeft(scrollBehavior?: ScrollBehavior): void;
+    scrollToRight(scrollBehavior?: ScrollBehavior): void;
     getScrollLeft(): number;
     getScrollTop(): number;
     getScrollWidth(): number;
